b0c1564b832a8290946539c662206d819340f4a7
medjmalami <jmalmohamedamine1@gmail.com>
2026-08-08 19:14:05 +0100

    feat: allow access for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["sb-58ch088vvg7h.vercel.run"],
+  },
   plugins: [react()],
   resolve: {
     alias: {
